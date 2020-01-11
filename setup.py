7a6b12d1cce5642d43b341ad12b70de1f6f019e3
Samuel Cook <scook@esri.com>
2020-01-11 09:54:21 -0600

    fix pkg name

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="geofactory-scook",
-    version="0.0.2",
+    name="geofactory",
+    version="0.0.1",
     author="Samuel Cook",
     author_email="rigdoncook@gmail.com",
     description="A simple geojson provider for faker",
