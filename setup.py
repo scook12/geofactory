eff6b0c10fead7b9d694565a3e83873305c23e31
Samuel Cook <scook@esri.com>
2020-01-11 10:09:13 -0600

    New minor pypi version to fix setup typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geofactory",
-    version="0.0.1",
+    version="0.0.2",
     author="Samuel Cook",
     author_email="rigdoncook@gmail.com",
     description="A simple geojson provider for faker",
